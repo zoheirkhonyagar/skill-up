053aab5f7c98b994152be5cabe682f106c48993c
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2022-08-19 14:36:55 +0430

    feat: added two microservices

---
+++ 01-node-js-microservices/speakers-service/server/service.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const service = express();
+
+module.exports = (config) => {
+  const log = config.log();
+  // Add a request logging middleware in development mode
+  if (service.get('env') === 'development') {
+    service.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return service;
+};
